ca670551421f4075781706d9370de2e102406700
Kevin Zhao <kevinzs2048@gmail.com>
2018-03-06 16:18:47 +0800

    Move capsule from experimental to V1
    So change the try case for it
    
    Signed-off-by: Kevin Zhao <kevinzs2048@gmail.com>

--- gophercloud/test-zun-specify-parameters.go
+++ gophercloud/test-zun-specify-parameters.go
@@ -4,15 +4,16 @@ import (
   "fmt"
   "github.com/gophercloud/gophercloud"
   "github.com/gophercloud/gophercloud/openstack"
-  "github.com/gophercloud/gophercloud/openstack/container/experimental/capsules"
+  "github.com/gophercloud/gophercloud/openstack/container/v1/capsules"
 )
 
+// TenantID is the project ID of your user(if your user is admin, then choose the admin user ID)
 func main() {
         opts := gophercloud.AuthOptions{
 		IdentityEndpoint: "http://10.169.41.188/identity",
 		Username: "admin",
 		Password: "password",
-		TenantID: "279b987f9ea2449b9f98fd94fb700fd8",
+		TenantID: "4036ad025daf4fcbb2c0cde3d6e59073",
 		DomainID: "default",
 	}
 	provider, err := openstack.AuthenticatedClient(opts)
@@ -22,7 +23,7 @@ func main() {
 		return
 	}
 	fmt.Println(opts)
-	client, err := openstack.NewContainerExperimental(provider, gophercloud.EndpointOpts{
+	client, err := openstack.NewContainerV1(provider, gophercloud.EndpointOpts{
 		Region: "RegionOne",
 	})
         if err != nil {
@@ -30,7 +31,7 @@ func main() {
 		return
 	}
 	fmt.Println(opts)
-	ret := capsules.Get(client, "e6c913bb-b4e4-409d-8b71-3e029f196458")
+	ret := capsules.Get(client, "871966da-386d-4a76-bc2a-3cd0032dc267")
 	server, id := ret.Extract()
 	fmt.Println("====================")
 	fmt.Println(id)
