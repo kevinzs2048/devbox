7474463ff61cfc090b1ed506fe6e4900648a44a2
Kevin Zhao <kevinzs2048@gmail.com>
2018-04-12 15:53:04 +0800

    Modify capsule create
    Signed-off-by: Kevin Zhao <kevinzs2048@gmail.com>

--- gophercloud/test-create.go
+++ gophercloud/test-create.go
@@ -13,7 +13,7 @@ func main() {
 		IdentityEndpoint: "http://10.169.41.188/identity",
 		Username: "admin",
 		Password: "password",
-		TenantID: "4036ad025daf4fcbb2c0cde3d6e59073",
+		TenantID: "01821bd38f2f474489491adb0da7efaf",
 		DomainID: "default",
 	}
 	provider, err := openstack.AuthenticatedClient(opts)
@@ -32,16 +32,13 @@ func main() {
 	}
 	fmt.Println(opts)
         template := new(capsules.Template)
-	template.Bin = []byte(`{"kind": "capsule", "metadata": {"labels": {"app": "web", "app1": "web1"}, "name": "template"}, "spec": {"containers": [{"workDir": "/root", "image": "ubuntu", "volumeMounts": [{"readOnly": true, "mountPath": "/data1", "name": "volume01"}], "command": ["/bin/bash"], "env": {"ENV2": "/usr/bin", "ENV1": "/usr/local/bin"}, "imagePullPolicy": "ifnotpresent", "ports": [{"containerPort": 80, "protocol": "TCP", "name": "nginx-port", "hostPort": 80}], "resources": {"requests": {"cpu": 1, "memory": 1024}}}, {"workDir": "/root", "image": "centos", "args": ["-c", "\"while true; do echo hello world; sleep 1; done\""], "volumeMounts": [{"mountPath": "/data2", "name": "volume02"}], "command": ["/bin/bash"], "env": {"ENV2": "/usr/bin/"}, "imagePullPolicy": "ifnotpresent", "ports": [{"containerPort": 80, "protocol": "TCP", "name": "nginx-port", "hostPort": 80}, {"containerPort": 3306, "protocol": "TCP", "name": "mysql-port", "hostPort": 3306}], "resources": {"requests": {"cpu": 1, "memory": 1024}}}], "volumes": [{"cinder": {"autoRemove": true, "size": 5}, "name": "volume01"}, {"cinder": {"autoRemove": true, "size": 5}, "name": "volume02"}]}, "restartPolicy": "Always", "capsuleVersion": "beta"}`)
+	template.Bin = []byte(`{"kind": "capsule", "metadata": {"labels": {"app": "web", "app1": "web1"}, "name": "test01"}, "spec": {"containers": [{"workDir": "/root", "image": "ubuntu", "volumeMounts": [{"readOnly": true, "mountPath": "/data1", "name": "volume11"}], "command": ["/bin/bash"], "env": {"ENV2": "/usr/bin", "ENV1": "/usr/local/bin"}, "imagePullPolicy": "ifnotpresent", "ports": [{"containerPort": 80, "protocol": "TCP", "name": "nginx-port", "hostPort": 80}], "resources": {"requests": {"cpu": 1, "memory": 1024}}}, {"workDir": "/root", "image": "centos", "args": ["-c", "\"while true; do echo hello world; sleep 1; done\""], "volumeMounts": [{"mountPath": "/data2", "name": "volume12"}], "command": ["/bin/bash"], "env": {"ENV2": "/usr/bin/"}, "imagePullPolicy": "ifnotpresent", "ports": [{"containerPort": 80, "protocol": "TCP", "name": "nginx-port", "hostPort": 80}, {"containerPort": 3306, "protocol": "TCP", "name": "mysql-port", "hostPort": 3306}], "resources": {"requests": {"cpu": 1, "memory": 1024}}}], "volumes": [{"cinder": {"autoRemove": true, "size": 5}, "name": "volume11"}, {"cinder": {"autoRemove": true, "size": 5}, "name": "volume12"}]}, "restartPolicy": "Always", "capsuleVersion": "beta"}`)
         createOpts := capsules.CreateOpts{
                 TemplateOpts:    template,
         }
 	ret := capsules.Create(client, createOpts)
-	server, id := ret.Extract()
 	fmt.Println("====================")
-	fmt.Println(id)
 	fmt.Println(ret)
 	fmt.Println("====================")
-	fmt.Println(server)
 }
 
